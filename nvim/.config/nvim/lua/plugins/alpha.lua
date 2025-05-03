70e4ee929e0bb7be517c6b4cf87e041e66549469
Indy <Indy@debian.org>
2025-05-03 11:39:09 -0400

    did a decent redo of some stuff

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -1,44 +1,44 @@
 ---@diagnostic disable: undefined-field
 return {
-	"goolord/alpha-nvim",
-	event = "VimEnter",
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  config = function()
+    local alpha = require "alpha"
+    local dashboard = require "alpha.themes.dashboard"
 
-		-- Set header
-		dashboard.section.header.val = {
-			"                                                     ",
-			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-			"                                                     ",
-		}
-		-- Set menu
-		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New File", ":AskNewFileName<CR>"),
-			dashboard.button("SPC ee", "  > File explorer", ":Oil --float<CR>"),
-			dashboard.button("SPC ff", "󰱼  > Find File", ":Telescope find_files<CR>"),
-			dashboard.button("SPC fs", "󰅳  > Find String", ":Telescope live_grep<CR>"),
-			dashboard.button("SPC fh", "󰞋  > Find Help", ":Telescope help_tags<CR>"),
-			dashboard.button("ce", "  > Config Nvim", ":OilDir $XDG_CONFIG_HOME/nvim<CR>"),
-			dashboard.button("ct", "  > Config Alacritty", ":EditFile ~/.config/alacritty/ alacritty.toml<CR>"),
-			dashboard.button("cs", "  > Config Zsh", ":EditFile ~ .zshrc<CR>"),
-			dashboard.button("cp", "  > Config OhMyPosh", ":EditFile ~/.config/ohmyposh/ base.toml<CR>"),
-			dashboard.button("cf", "󰌢  > Config FastFetch", ":EditFile ~/.config/fastfetch/ config.jsonc<CR>"),
-			dashboard.button("cy", "󰇥  > Config Yazi", ":OilDir $XDG_CONFIG_HOME/yazi<CR>"),
-			dashboard.button("md", "  > Opens My Markdown Folder", ":OilDir $HOME/Documents/Markdown<CR>"),
-			dashboard.button("q", "  > Quit Nvim", ":qa!<CR>"),
-			dashboard.button("SPC l", "󰒲  > Lazy", ":Lazy<CR>"),
-		}
+    -- Set header
+    dashboard.section.header.val = {
+      "                                                     ",
+      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+      "                                                     ",
+    }
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  > New File", ":AskNewFileName "),
+      dashboard.button("SPC ee", "  > File explorer", ":Oil --float<CR>"),
+      dashboard.button("SPC ff", "󰱼  > Find File", ":Telescope find_files<CR>"),
+      dashboard.button("SPC fs", "󰅳  > Find String", ":Telescope live_grep<CR>"),
+      dashboard.button("SPC fh", "󰞋  > Find Help", ":Telescope help_tags<CR>"),
+      dashboard.button("ce", "  > Config Nvim", ":OilDir $XDG_CONFIG_HOME/nvim<CR>"),
+      dashboard.button("ct", "  > Config Alacritty", ":EditFile ~/.config/alacritty/ alacritty.toml<CR>"),
+      dashboard.button("cs", "  > Config Zsh", ":EditFile ~ .zshrc<CR>"),
+      dashboard.button("cp", "  > Config OhMyPosh", ":EditFile ~/.config/ohmyposh/ base.toml<CR>"),
+      dashboard.button("cf", "󰌢  > Config FastFetch", ":EditFile ~/.config/fastfetch/ config.jsonc<CR>"),
+      dashboard.button("cy", "󰇥  > Config Yazi", ":OilDir $XDG_CONFIG_HOME/yazi<CR>"),
+      dashboard.button("md", "  > Opens My Markdown Folder", ":OilDir $HOME/Documents/Markdown<CR>"),
+      dashboard.button("q", "  > Quit Nvim", ":qa!<CR>"),
+      dashboard.button("SPC l", "󰒲  > Lazy", ":Lazy<CR>"),
+    }
 
-		-- Send config to alpha
-		alpha.setup(dashboard.opts)
+    -- Send config to alpha
+    alpha.setup(dashboard.opts)
 
-		-- Disable folding on alpha buffer
-		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-	end,
+    -- Disable folding on alpha buffer
+    vim.cmd [[autocmd FileType alpha setlocal nofoldenable]]
+  end,
 }
