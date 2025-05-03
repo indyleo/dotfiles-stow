987b8d278e34583d265cd3fe6c86f3d324f14b2e
Indy <Indy@debian.org>
2025-05-03 11:00:42 -0400

    doing some stuff to get vim lib to be gloabal idk whats going on

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -1,81 +1,81 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    -- import mason
+    local mason = require "mason"
 
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
+    -- import mason-lspconfig
+    local mason_lspconfig = require "mason-lspconfig"
 
-		local mason_tool_installer = require("mason-tool-installer")
+    local mason_tool_installer = require "mason-tool-installer"
 
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+    -- enable mason and configure icons
+    mason.setup {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    }
 
-		mason_lspconfig.setup({
-			ensure_installed = {
-				-- Text
-				"jsonls",
-				"yamlls",
-				"taplo",
-				"ltex",
+    mason_lspconfig.setup {
+      ensure_installed = {
+        -- Text
+        "jsonls",
+        "yamlls",
+        "taplo",
+        "ltex",
 
-				-- Web
-				"html",
-				"cssls",
-				"eslint",
+        -- Web
+        "html",
+        "cssls",
+        "eslint",
 
-				-- Langs
-				"pyright",
-				"lua_ls",
-				"gopls",
+        -- Langs
+        "pyright",
+        "lua_ls",
+        "gopls",
 
-				-- Script/Shell
-				"powershell_es",
-				"bashls",
-			},
-		})
+        -- Script/Shell
+        "powershell_es",
+        "bashls",
+      },
+    }
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				-- Text
-				"yamllint",
-				"jsonlint",
-				"markdownlint-cli2",
-				"codespell",
+    mason_tool_installer.setup {
+      ensure_installed = {
+        -- Text
+        "yamllint",
+        "jsonlint",
+        "markdownlint-cli2",
+        "codespell",
 
-				-- Web
-				"htmlhint",
-				"stylelint",
-				"eslint_d",
+        -- Web
+        "htmlhint",
+        "stylelint",
+        "eslint_d",
 
-				-- Langs
-				"prettier",
-				"stylua",
-				"isort",
-				"black",
-				"pylint",
-				"selene",
-				"crlfmt",
-				"staticcheck",
+        -- Langs
+        "prettier",
+        "stylua",
+        "isort",
+        "black",
+        "pylint",
+        "selene",
+        "crlfmt",
+        "staticcheck",
 
-				-- Script/Shell
-				"beautysh",
-				"shellcheck",
-				"shellharden",
-			},
-		})
-	end,
+        -- Script/Shell
+        "beautysh",
+        "shellcheck",
+        "shellharden",
+      },
+    }
+  end,
 }
