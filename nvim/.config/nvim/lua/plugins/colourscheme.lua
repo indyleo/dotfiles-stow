9cf8b8474086c8c9eb9386a80121a95f9a41350f
Indy <Indy@debian.org>
2025-05-23 18:45:54 -0400

    big theme changes

--- nvim/.config/nvim/lua/plugins/colourscheme.lua
+++ nvim/.config/nvim/lua/plugins/colourscheme.lua
@@ -1,17 +1,1 @@
-return {
-  "gbprod/nord.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    if vim.g.neovide then
-      require("nord").setup {
-        transparent = false,
-      }
-    else
-      require("nord").setup {
-        transparent = true,
-      }
-    end
-    vim.cmd.colorscheme "nord"
-  end,
-}
+/home/indy/.config/nvim/lua/plugins/colourscheme.lua_gruvbox
