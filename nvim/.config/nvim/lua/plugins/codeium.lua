5c5194bf976c5a4d68caac46396d324df99d71d6
Indy <Indy@debian.org>
2025-06-11 10:37:33 -0400

    no more ai in text files only code

--- nvim/.config/nvim/lua/plugins/codeium.lua
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -1,5 +1,5 @@
 return {
-  "Exafunction/codeium.nvim",
+  "Exafunction/windsurf.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "hrsh7th/nvim-cmp",
@@ -10,6 +10,13 @@ return {
       enable_cmp_source = false,
       virtual_text = {
         enabled = true,
+        filetypes = {
+          markdown = false,
+          text = false,
+          gitcommit = false,
+          gitrebase = false,
+        },
+        default_filetype_enabled = true,
         virtual_text_priority = 65535,
         map_keys = true,
         key_bindings = {
