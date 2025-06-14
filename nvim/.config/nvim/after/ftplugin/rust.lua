2415ff981a45b6eddea7bafa1a8e28fd3ca48708
Indy <Indy@debian.org>
2025-06-14 01:20:24 -0400

    made rust dev easier

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -7,9 +7,40 @@ vim.api.nvim_create_autocmd("BufEnter", {
     local rsopts = function(desc)
       return { desc = desc, buffer = bufnr, noremap = true, silent = true }
     end
-    keymap("n", "<leader>a", function()
+    local function cargo_cmd(cmd)
+      vim.notify("🔧 Running cargo " .. cmd .. "...", vim.log.levels.INFO, { title = "Cargo" })
+
+      local output = {}
+
+      vim.fn.jobstart({ "cargo", cmd }, {
+        stdout_buffered = true,
+        stderr_buffered = true,
+
+        on_stdout = function(_, data)
+          if data then
+            vim.list_extend(output, data)
+          end
+        end,
+
+        on_stderr = function(_, data)
+          if data then
+            vim.list_extend(output, data)
+          end
+        end,
+
+        on_exit = function(_, code)
+          local msg = table.concat(output, "\n")
+
+          if code == 0 then
+            vim.notify("✅ cargo " .. cmd .. " succeeded:\n" .. msg, vim.log.levels.INFO, { title = "Cargo" })
+          else
+            vim.notify("❌ cargo " .. cmd .. " failed:\n" .. msg, vim.log.levels.ERROR, { title = "Cargo" })
+          end
+        end,
+      })
+    end
+    keymap("n", "<leader>ra", function()
       vim.cmd.RustLsp "codeAction" -- supports rust-analyzer's grouping
-      -- or vim.lsp.buf.codeAction() if you don't want grouping.
     end, rsopts "Code actions")
     keymap(
       "n",
@@ -19,5 +50,14 @@ vim.api.nvim_create_autocmd("BufEnter", {
       end,
       rsopts "Hover actions"
     )
+    keymap("n", "<leader>rb", function()
+      cargo_cmd "build"
+    end, rsopts "Cargo build")
+    keymap("n", "<leader>rr", function()
+      cargo_cmd "run"
+    end, rsopts "Cargo run")
+    keymap("n", "<leader>rc", function()
+      cargo_cmd "check"
+    end, rsopts "Cargo check")
   end,
 })
