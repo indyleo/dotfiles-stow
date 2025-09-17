5e307f82c8f61e7d0395943c60e8c1542fdb14c2
Indy <Indy@debian.org>
2025-09-17 09:22:43 -0400

    fix

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -40,7 +40,6 @@ return {
       -- sources for autocompletion
       sources = cmp.config.sources {
         { name = "nvim_lsp" },
-        { name = "lazydev" },
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
