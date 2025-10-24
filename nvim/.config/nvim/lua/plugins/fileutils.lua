5c9ef59f36e86c97dfae28e1147273bebd953897
Indy <Indy@debian.org>
2025-10-24 08:47:23 -0400

    rm oil

--- nvim/.config/nvim/lua/plugins/fileutils.lua
+++ nvim/.config/nvim/lua/plugins/fileutils.lua
@@ -1,14 +1,5 @@
 return {
   "indyleo/fileutils-nvim",
-  dependencies = {
-    {
-      "stevearc/oil.nvim",
-      dependencies = {
-        "nvim-tree/nvim-web-devicons",
-      },
-      lazy = false,
-    },
-  },
   lazy = false,
   config = true,
   -- { -- Here to test fileutils
