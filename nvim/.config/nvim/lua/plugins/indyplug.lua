8b047e9ee8b2f3c2a3a1781807e6c2c1920a17e0
Indy <Indy@debian.org>
2025-11-03 09:46:23 -0500

    some moving

---
+++ nvim/.config/nvim/lua/plugins/indyplug.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "indyleo/sword-nvim",
+    lazy = false,
+    config = true,
+  },
+  {
+    "indyleo/fileutils-nvim",
+    lazy = false,
+    config = true,
+  },
+  -- { -- Here to test fileutils
+  --   dir = "~/Github/fileutils-nvim",
+  --   lazy = false,
+  --   config = true,
+  -- },
+
+  -- { -- Here to test sword
+  --   dir = "~/Github/sword-nvim",
+  --   lazy = false,
+  --   config = true,
+  -- },
+}
