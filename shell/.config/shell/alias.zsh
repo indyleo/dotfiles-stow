6bc85a4749147b92c4ae279ab63e0b190dd40f48
Indy <Indy@debian.org>
2025-11-07 00:22:04 -0500

    ns

--- shell/.config/shell/alias.zsh
+++ shell/.config/shell/alias.zsh
@@ -140,7 +140,8 @@ fi
 # Random Aliases
 alias tmodserver="~/.steam/debian-installation/steamapps/common/tModLoader/start-tModLoaderServer.sh"
 alias tmodport="ngrok tcp 7777"
-alias fusee="sudo fusee-nano ~/Downloads/Bin/hekate_ctcaer_*.bin"
+alias fusee="sudo fusee-nano ~/Bin/hekate_ctcaer_*.bin"
+alias ns="java -jar ~/Code/ns-usbloader-*.jar"
 
 # For Vim Users
 # vim:ft=sh
