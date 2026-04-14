1a30c44c078fcfb32ad289d5f173e1ea9037aae1
Indy <Indy@debian.org>
2026-04-13 20:41:43 -0400

    there

--- hypr/.config/hypr/scripts/portal-hypr.sh
+++ hypr/.config/hypr/scripts/portal-hypr.sh
@@ -1,7 +1,11 @@
 #!/bin/env sh
 sleep 1
 killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-gtk
 killall xdg-desktop-portal
+sleep 1
 /usr/lib/xdg-desktop-portal-hyprland &
-sleep 2
+sleep 1
+/usr/lib/xdg-desktop-portal-gtk &
+sleep 1
 /usr/lib/xdg-desktop-portal &
