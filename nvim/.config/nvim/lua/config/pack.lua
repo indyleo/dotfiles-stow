28b15099b5a3d387e44b065141835b0878f187d8
Indy <Indy@debian.org>
2026-09-13 12:19:30 -0400

    fix: icons

--- nvim/.config/nvim/lua/config/pack.lua
+++ nvim/.config/nvim/lua/config/pack.lua
@@ -332,10 +332,10 @@ load("nvim-lspconfig", function()
   vim.diagnostic.config {
     signs = {
       text = {
-        [vim.diagnostic.severity.ERROR] = "",
-        [vim.diagnostic.severity.WARN] = "",
-        [vim.diagnostic.severity.HINT] = "󰠠",
-        [vim.diagnostic.severity.INFO] = "",
+        [vim.diagnostic.severity.ERROR] = "󰅚",
+        [vim.diagnostic.severity.WARN] = "󰀪",
+        [vim.diagnostic.severity.HINT] = "",
+        [vim.diagnostic.severity.INFO] = "",
       },
     },
     update_in_insert = true,
